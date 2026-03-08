16f26e72efa8eddd1cac6ae2ec8bf6c68783a9ef
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2026-03-07 22:05:50 -0300

    añado uso de iconos de bootstrap y modifico el navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <nav className='flex items-center justify-between w-full h-[5em] bg-cyan-950 mx-auto z-30 py-5 px-6 lg:px-20 2xl:px-0'>
         <a href="/">
-            <img src={logo} alt="logo provisorio" style={{width: '90px', height: '40px'}}/>
+            <img src={logo} alt="logo libroteca" style={{width: '90px', height: '40px'}}/>
         </a>
         {/*navegación en desktop*/}
         <ul className="invisible h-full gap-12 lg:flex lg:visible">
@@ -19,6 +19,10 @@ const Navbar = () => {
           }          
         </ul>
 
+        <div className=' bg-blue-900 w-12 h-12 p-2 text-green-300 rounded-full flex items-center justify-center border-2 border-white
+         hover:bg-white hover:text-green-600'>
+          <a href="/" className='inline-block w-full h-full text-center'><i className="bi bi-basket2"></i></a>
+        </div>
         {/*navegación en mobile*/}
         <BurgerMenu/>
         
